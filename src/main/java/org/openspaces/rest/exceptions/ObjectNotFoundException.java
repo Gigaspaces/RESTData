5da580463f7487ddcff0eb9e31dd991b8bafaa07
yohana <yohaha@yohanakhoury.com>
2015-02-09 16:30:59 +0200

    GS-12126 Added license in the top of each class

--- src/main/java/org/openspaces/rest/exceptions/ObjectNotFoundException.java
+++ src/main/java/org/openspaces/rest/exceptions/ObjectNotFoundException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 GigaSpaces Technologies Ltd
+ * Copyright 2015 GigaSpaces Technologies Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
